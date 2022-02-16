62183e3cc11ae63557399839010db4515df784ce
rianmfir <rianmfirdaus@gmail.com>
2022-02-17 04:43:02 +0700

    Menerapkan redux pada counter sederhana

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
 import './App.css';
+import Redux from './Redux';
+import store from './Redux/app/store';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Provider store={store}>
+          <Redux />
+        </Provider>
       </header>
     </div>
   );
